7f4ea4306867c6a870f523587f44a2c5000aed0d
Seb0927 <sebastianidrobo@hotmail.com>
2024-07-04 15:08:26 -0500

    feat: header component mockup added

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -6,16 +6,18 @@ import ChevronRight from '../../assets/vectors/chevron-right.svg?react';
 const Background = ({ children }) => {
   return (
     <motion.div className="bg-green-lightest h-screen w-screen overflow-hidden">
-      {/* Header with distinct styling */}
-      <div className="text-black w-full p-20 items flex justify-center">
-        <Header></Header>
+      {/* Header */}
+      <div className="text-black w-full pt-16 pb-16 items flex justify-center">
+        <Header sections={['Home', 'Projects', 'Experience', 'Contact']}></Header>
       </div>
-      {/* Content area with padding and grid layout */}
+
+      {/* Content area */}
       <div className="px-20 grid grid-cols-13 gap-2 items-center place-content-start h-full">
         {/* Arrow */}
         <div className="flex justify-start">
           <ChevronLeft className="fill-current text-green-dark h-2/6 w-2/6"/>
         </div>
+        {/* Content */}
         {children}
         {/* Arrow */}
         <div className="flex justify-end">
--- src/components/background/Header.jsx
+++ src/components/background/Header.jsx
@@ -1,6 +1,41 @@
-function Header() {
+function Header(props) {
+  console.log(props.sections)
+
+  const sections = props.sections.map((section) => {
+    return (
+      <div key={section} className="flex justify-center items-center text-lg p-2">
+        {section}
+      </div>
+    )
+  })
+
+  const backgrounds = props.sections.map((section) => {
+    if (section == 'Home'){
+      return (
+        <div className="h-1.5 bg-green-dark">
+        </div>
+      )
+    }
+    
+    return (
+      <div className="h-1.5 bg-green-light">
+      </div>
+    )
+  })
+
+  console.log(props.sections.length)
+
   return (
-    <div>Header</div>
+    <>
+      <div>
+        <div className={`grid grid-cols-${props.sections.length} gap-2`}>
+          {sections}
+        </div>
+        <div className={`grid grid-cols-${props.sections.length}`}>
+          {backgrounds}
+        </div>
+      </div>
+    </>
   )
 }
 
