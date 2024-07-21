5889b631625b55d5b051a294f4a62da8223dfcb5
Seb0927 <sebastianidrobo@hotmail.com>
2024-07-21 12:21:30 -0500

    feat: add header animation for medium-sized screens.
    - Grids on head were changed to flex box because they are more versatile.
    - I lost too much time because of the above reason :(

--- src/components/background/Header.jsx
+++ src/components/background/Header.jsx
@@ -1,10 +1,10 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { useWindowDimensions}  from '../../hooks'
-import { gridColumnsVariants, bgDarkColorsVariants, bgLightColorsVariants, bgLightColorsValues, bgDarkColorsValues} from '../../utils'
+import { useWindowDimensions } from '../../hooks'
+import { gridColumnsVariants, bgDarkColorsVariants, bgLightColorsVariants, bgLightColorsValues, bgDarkColorsValues } from '../../utils'
 
 function Header(props) {
   // Component variables
-  const { sections, currentSection, handleSection } = props; 
+  const { sections, currentSection, handleSection } = props;
   const index = sections.indexOf(currentSection)
 
   // Amount of sections to render
@@ -12,22 +12,26 @@ function Header(props) {
   const isSmallScreen = width < 768;
 
   const slicingSections = (index) => {
-    if (index == 0){
+    if (index == 0) {
       return sections.slice(index, index + 3)
     } else if (index == sections.length - 1) {
-      return sections.slice(index-2, index+1)
+      return sections.slice(index - 2, index + 1)
     }
-      
-    return sections.slice(index-1, index+2)
+
+    return sections.slice(index - 1, index + 2)
   }
 
   const sectionsToRender = isSmallScreen ? slicingSections(index) : sections;
 
   const sectionsReturn = sectionsToRender.map((section) => {
     return (
-      <button key={section} onClick={() => handleSection(section)} className="flex justify-center items-center text-lg p-2">
+      <motion.button key={section} onClick={() => handleSection(section)} className="flex-1 justify-center text-lg p-2"
+        initial={{ opacity: 0, transition: { delay: 0.5} }}
+        animate={{ opacity: 1, transition: { delay: 0.5 } }}
+        exit={{ opacity: 0, transition: { delay: 0 } }} 
+        layoutId={section}>
         {section}
-      </button>
+      </motion.button>
     )
   })
 
@@ -45,28 +49,30 @@ function Header(props) {
   const backgrounds = sectionsToRender.map((section) => {
     const isCurrentSection = section === currentSection;
     return (
-      <div key={section} className="relative">
-        <motion.div className={`h-1.5 ${bgLightColorsVariants[index]}`} variants={lightVariants} animate={"newColor"}></motion.div>
-        <AnimatePresence>
+      <div key={section} className="flex-1 justify-center h-1.5">
+        <motion.div className={`h-1.5 ${bgLightColorsVariants[index]}`} variants={lightVariants} animate={"newColor"}>
+          <AnimatePresence mode="sync">
           {isCurrentSection && <motion.div
             key={"current-section"}
             layoutId="current-section"
-            className={`h-1.5 ${bgDarkColorsVariants[index]} z-1`}
-            style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 1}}
+            className={`h-1.5 ${bgDarkColorsVariants[index]} relative z-1`}
           />}
         </AnimatePresence>
-      </div> 
+        </motion.div>
+      </div>
     )
   })
 
   return (
-    <div>
+    <div className="w-88 md:w-120">
       {/* Header Sections */}
-      <div className={`grid gap-2 ${gridColumnsVariants[sectionsToRender.length]}`}>
-        {sectionsReturn}
+      <div className="flex flex-row">
+        <AnimatePresence mode="popLayout" initial={false}>
+          {sectionsReturn}
+        </AnimatePresence>
       </div>
       {/* Bottom Bar Header */}
-      <div className={`grid ${gridColumnsVariants[sectionsToRender.length]}`}>
+      <div className="flex flex-row">
         {backgrounds}
       </div>
     </div>
