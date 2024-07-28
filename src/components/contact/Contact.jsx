6789aa7d6faabd50e820d271ded8847592ffdc1e
Seb0927 <sebastianidrobo@hotmail.com>
2024-07-27 22:51:25 -0500

    feat: contact component mockup done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,20 @@
 import { useEffect } from "react";
 import { motion } from "framer-motion";
+import { useWindowDimensions } from '../../hooks'
+import Linkedin from '../../assets/vectors/linkedin.svg?react';
+import Github from '../../assets/vectors/github.svg?react';
 
 function Contact({ hasRendered, setHasRendered, sectionIndex }) {
 
   useEffect(() => {
-    const updatedRenderedState = [...hasRendered];
-    updatedRenderedState[sectionIndex] = true;
-    setHasRendered(updatedRenderedState);
+    const updatedRenderedState = [...hasRendered]
+    updatedRenderedState[sectionIndex] = true
+    setHasRendered(updatedRenderedState)
   }, [])
+
+  // Amount of sections to render
+  const { width, height } = useWindowDimensions();
+  const isSmallScreen = width < 768;
 
   // Framer variables
   let renderVariants = null;
@@ -25,7 +32,7 @@ function Contact({ hasRendered, setHasRendered, sectionIndex }) {
         }
       },
     }
-    
+
   } else {
     renderVariants = {
       hiddenUp: { opacity: 0 },
@@ -36,52 +43,54 @@ function Contact({ hasRendered, setHasRendered, sectionIndex }) {
   }
 
   return (
-    <div className="md:px-16 md:py-4 md:flex md:justify-center md:h-5/6">
-      <section>
-        <motion.h1 className="text-8xl text-center font-bold mb-4"
-        variants={renderVariants}
-        initial="hiddenUp"
-        animate="visible"
-        custom={1}>WIP</motion.h1>
-        <motion.p className="text-xl text-center"
-        variants={renderVariants}
-        initial="hiddenUp"
-        animate="visible"
-        custom={2}>This section is still under construction! Sorry for the inconvenience</motion.p>
-        <motion.div className="flex justify-center items-center"
-        variants={renderVariants}
-        initial="hiddenUp"
-        animate="visible"
-        custom={3}>
-          <pre className="text-[8px] w-auto justify-center md:text-[12px]">
-          {`
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠋⠈⠻⣮⣳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⡿⠋⠀⠀⠀⠀⠙⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⡿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠈⠛⠛⠿⠿⣿⣷⣶⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⡿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠻⠿⣿⣶⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⣀⣠⣤⣤⣀⡀⠀⠀⣀⣴⣿⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣄⠀⠀
-⢀⣤⣾⡿⠟⠛⠛⢿⣿⣶⣾⣿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣷⣦⣀⣀⣤⣶⣿⡿⠿⢿⣿⡀⠀
-⣿⣿⠏⠀⢰⡆⠀⠀⠉⢿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⡿⠟⠋⠁⠀⠀⢸⣿⠇⠀
-⣿⡟⠀⣀⠈⣀⡀⠒⠃⠀⠙⣿⡆⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠇⠀
-⣿⡇⠀⠛⢠⡋⢙⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀
-⣿⣧⠀⠀⠀⠓⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠋⠀⠀⢸⣧⣤⣤⣶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡿⠀⠀
-⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠻⣷⣶⣶⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠁⠀⠀
-⠈⠛⠻⠿⢿⣿⣷⣶⣦⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⡏⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠻⠿⢿⣿⣷⣶⣦⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⡄⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠛⠻⠿⢿⣿⣷⣶⣦⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⡄⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠿⠿⣿⣷⣶⣶⣤⣤⣀⡀⠀⠀⠀⢀⣴⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡿⣄
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠿⠿⣿⣷⣶⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣹
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⠀⢸⣧
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣆⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣶⣾⣿⣿⣿⣿⣤⣄⣀⡀⠀⠀⠀⣿
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⣻⣷⣶⣾⣿⣿⡿⢯⣛⣛⡋⠁⠀⠀⠉⠙⠛⠛⠿⣿⣿⡷⣶⣿
-          `}
-          </pre>
-        </motion.div>
-        <div className="h-56 md:h-0" />
-        <p className="text-sm text-center md:h-0 md:text-[0px]">(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ✧ﾟ･: *ヽ(◕ヮ◕ヽ)</p>
-        {/*https://emojicombos.com/cute-ascii-art */}
-      </section>
+    <div className="md:h-5/6">
+      <h1 className="mb-8 col-span-2 text-5xl font-semibold text-center md:py-8">More about me?</h1>
+      <div className="md:flex md:flex-row md:gap-12 lg:gap-24">
+        <section className="flex-1">
+          <p className="mb-4 text-lg">You can find more about me on the following sites! I will be happy to connect with you</p>
+          <div className="flex gap-6 mb-6 justify-center md:justify-start">
+            <a href="https://www.linkedin.com/in/sebastianidroboavirama/">
+              <Linkedin className="h-16 w-16 fill-green-dark" />
+            </a>
+            <a href="https://github.com/Seb0927/">
+              <Github className="h-16 w-16 fill-green-dark" />
+            </a>
+          </div>
+        </section>
+        {isSmallScreen ? <hr className="bg-green-dark h-1 mb-6" />: null}
+        <section className="flex-1">
+          <p className="text-lg mb-4">Or feel free to reach out me. No compromise:</p>
+          <form className="w-full">
+            <div className="flex flex-wrap lg:-mx-3 mb-6">
+              <div className="w-full lg:w-1/4 lg:px-3 lg:mb-0">
+                <label className="block text-sm italic mb-1" for="name">
+                  Name:
+                </label>
+                <input class="appearance-none block w-full bg-[#9ED5AD] text-black rounded py-3 px-4 mb-3 focus:outline-none focus:bg-[#AFEDC2]" id="name" type="text" />
+              </div>
+              <div class="w-full lg:w-3/4 lg:px-3 lg:mb-3">
+                <label class="block text-sm italic mb-1" for="email">
+                  Email:
+                </label>
+                <input class="appearance-none block w-full bg-[#9ED5AD] text-black rounded py-3 px-4 mb-3 focus:outline-none focus:bg-[#AFEDC2]" id="email" type="text" />
+              </div>
+              <div class="w-full lg:px-3">
+                <label class="block text-sm italic mb-1" for="message">
+                  Message:
+                </label>
+                <textarea class="h-36 min-h-12 max-h-56 appearance-none block w-full bg-[#9ED5AD] text-black rounded py-3 px-4 mb-6 focus:outline-none focus:bg-[#AFEDC2]" id="message" type="text" />
+              </div>
+              <div className="w-full flex justify-center mx-3">
+                <button onClick={() => alert("Message sent!")} type="button"
+                className="appearance-none block bg-[#9ED5AD] text-black font-bold rounded-full py-2 px-12  hover:bg-[#AFEDC2]"
+                >
+                  Send
+                </button>
+              </div>
+            </div>
+          </form>
+        </section>
+      </div>
     </div>
   )
 }
