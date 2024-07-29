d61ed4d69535efd3457a393c021694def2030e8d
Seb0927 <sebastianidrobo@hotmail.com>
2024-07-29 00:04:28 -0500

    feat: form sends an email
    - TO-DO: To implement a server in order to fetch.

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,27 +1,33 @@
 function Form() {
+  // Component variables
+  const formActionUrl = `https://formsubmit.co/${import.meta.env.VITE_FORM_EMAIL}`;
+  // The line above is not safe since the value is still being saved on the client side.
+  // You should use a server from where you can fetch the value and use it in the form.
+  // I hope to change that on the future
+
   return (
-    <form className="w-full">
+    <form className="w-full" action={formActionUrl} method="POST">
       <div className="flex flex-wrap lg:-ml-3 mb-6">
         <div className="w-full lg:w-1/4 lg:px-3 lg:mb-0">
           <label className="block text-sm italic mb-1" for="name">
             Name:
           </label>
-          <input class="appearance-none block w-full bg-[#9ED5AD] text-black rounded py-3 px-4 mb-3 focus:outline-none focus:bg-[#AFEDC2]" id="name" type="text" />
+          <input class="appearance-none block w-full bg-[#9ED5AD] text-black rounded py-3 px-4 mb-3 focus:outline-none focus:bg-[#AFEDC2]" id="name" type="text" name="name"/>
         </div>
         <div class="w-full lg:w-3/4 lg:px-3 lg:mb-3">
           <label class="block text-sm italic mb-1" for="email">
             Email:
           </label>
-          <input class="appearance-none block w-full bg-[#9ED5AD] text-black rounded py-3 px-4 mb-3 focus:outline-none focus:bg-[#AFEDC2]" id="email" type="text" />
+          <input class="appearance-none block w-full bg-[#9ED5AD] text-black rounded py-3 px-4 mb-3 focus:outline-none focus:bg-[#AFEDC2]" id="email" type="text" name="email"/>
         </div>
         <div class="w-full lg:px-3">
           <label class="block text-sm italic mb-1" for="message">
             Message:
           </label>
-          <textarea class="h-36 min-h-12 max-h-56 appearance-none block w-full bg-[#9ED5AD] text-black rounded py-3 px-4 mb-6 focus:outline-none focus:bg-[#AFEDC2]" id="message" type="text" />
+          <textarea class="h-36 min-h-12 max-h-56 appearance-none block w-full bg-[#9ED5AD] text-black rounded py-3 px-4 mb-6 focus:outline-none focus:bg-[#AFEDC2]" id="message" type="text" name="message"/>
         </div>
         <div className="w-full flex justify-center mx-3">
-          <button onClick={() => alert("Message sent!")} type="button"
+          <button type="submit"
             className="appearance-none block bg-[#9ED5AD] text-black font-bold rounded-full py-2 px-12  hover:bg-[#AFEDC2]">
             Send
           </button>
